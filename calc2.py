8896be347a7ba8a5e4147ab87e352074e38cae43
Prachi Srivastava <prachisri8827@gmail.com>
2023-02-18 12:35:15 +0530

    game

--- calc2.py
+++ calc2.py
@@ -7,25 +7,25 @@ while True:
     print('3. Multiplication')
     print('4. Exit')
     match (input('enter your choice: ')):
-    case '1':
-        a = int(input('enter first number: '))
-        b = int(input('enter secod number: '))
-        print(f'{a} + {b} is {a+b}')
-    case '2':
-        a = int(input('enter first number: '))
-        b = int(input('enter secod number: '))
-        print(f'{a} - {b} is {a-b}')
-    case '3':
-        a = int(input('enter first number: '))
-        b = int(input('enter secod number: '))
-        print(f'{a} * {b} is {a*b}')
-    case '4':    
-        a = int(input('enter first number: '))
-        b = int(input('enter secod number: '))
-        print(f'{a} / {b} is {a/b}')
-        print('existing....')
-        break
-    case_:
-        print('Invalid Choice')
-    input('press enter to continue.....')
-    os.system('cls')    
+        case '1':
+            a = int(input('enter first number: '))
+            b = int(input('enter secod number: '))
+            print(f'{a} + {b} is {a+b}')
+        case '2':
+            a = int(input('enter first number: '))
+            b = int(input('enter secod number: '))
+            print(f'{a} - {b} is {a-b}')
+        case '3':
+            a = int(input('enter first number: '))
+            b = int(input('enter secod number: '))
+            print(f'{a} * {b} is {a*b}')
+        case '4':    
+            a = int(input('enter first number: '))
+            b = int(input('enter secod number: '))
+            print(f'{a} / {b} is {a/b}')
+            print('existing....')
+            break
+        case'_':
+            print('Invalid Choice')
+    input('press enter to continue....')
+    os.system('cls')        
